5c9edc385dda3e7cf5b200204bc7ee0670ced99c
Lana <110370@st-maartenscollege.nl>
2021-03-03 21:09:15 +0100

    Functie schrijven om gegevens op te halen

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,7 +6,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using MySql.Data; 
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace Bioscoop_Website_2021.Controllers
 {
@@ -21,10 +22,50 @@ namespace Bioscoop_Website_2021.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            // alle namen ophalen
+            var names = GetNames();
+
+            // stop de namen in de html
+            return View(names);
         }
 
-        public IActionResult Privacy()
+        public List<string> GetNames()
+        {
+            // stel in waar de database gevonden kan worden
+            string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=110370;Uid=110370;Pwd=inf2021sql;";
+
+            // maak een lege lijst waar we de namen in gaan opslaan
+            List<string> names = new List<string>();
+
+            // verbinding maken met de database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                // verbinding openen
+                conn.Open();
+
+                // SQL query die we willen uitvoeren
+                MySqlCommand cmd = new MySqlCommand("select * from product", conn);
+
+                // resultaat van de query lezen
+                using (var reader = cmd.ExecuteReader())
+                {
+                    // elke keer een regel (of eigenlijk: database rij) lezen
+                    while (reader.Read())
+                    {
+                        // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
+                        string Name = reader["Naam"].ToString();
+
+                        // voeg de naam toe aan de lijst met namen
+                        names.Add(Name);
+                    }
+                }
+            }
+
+            // return de lijst met namen
+            return names;
+        }
+
+public IActionResult Privacy()
         {
             return View();
         }
