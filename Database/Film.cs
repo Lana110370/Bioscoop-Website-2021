173ee5a69b3563a20fadcd0924a787f8b80e0562
Lana <110370@st-maartenscollege.nl>
2021-03-07 21:29:53 +0100

    Database object maken

---
+++ Database/Film.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bioscoop_Website_2021.Database
+{
+    public class Film
+    {
+        public int id { get; set; }
+
+        public int Titel { get; set; }
+
+        public int Leeftijdsgrens { get; set; }
+
+        public int Beschrijving { get; set; }
+
+    }
+}
