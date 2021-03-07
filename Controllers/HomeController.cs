08a10c2d490c10fdb9ec0723688da0801ac95fbc
Lana <110370@st-maartenscollege.nl>
2021-03-07 21:55:35 +0100

    Producten ophalen

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using MySql.Data;
 using MySql.Data.MySqlClient;
+using Bioscoop_Website_2021.Database;
 
 namespace Bioscoop_Website_2021.Controllers
 {
@@ -65,7 +66,49 @@ namespace Bioscoop_Website_2021.Controllers
             return names;
         }
 
-public IActionResult Privacy()
+        public List<Film> GetProducts()
+        {
+            // stel in waar de database gevonden kan worden
+            string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=110370;Uid=110370;Pwd=inf2021sql;";
+
+            // maak een lege lijst waar we de namen in gaan opslaan
+            List<Film> products = new List<Film>();
+
+            // verbinding maken met de database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                // verbinding openen
+                conn.Open();
+
+                // SQL query die we willen uitvoeren
+                MySqlCommand cmd = new MySqlCommand("select * from film", conn);
+
+                // resultaat van de query lezen
+                using (var reader = cmd.ExecuteReader())
+                {
+                    // elke keer een regel (of eigenlijk: database rij) lezen
+                    while (reader.Read())
+                    {
+                        Film p = new Film
+                        {
+                            // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
+                            id = Convert.ToInt32(reader["Id"]),
+                            Titel = Convert.ToInt32(reader["Titel"]),
+                            Leeftijdsgrens = Convert.ToInt32(reader["Leeftijdsgrens"]),
+                            Beschrijving = Convert.ToInt32(reader["Beschrijving"]),
+                        };
+
+                        // voeg de naam toe aan de lijst met namen
+                        products.Add(p);
+                    }
+                }
+            }
+
+            // return de lijst met namen
+            return products;
+        }
+
+        public IActionResult Privacy()
         {
             return View();
         }
