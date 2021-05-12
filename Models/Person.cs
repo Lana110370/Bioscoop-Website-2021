21dc4a9f44c5e243f74af32241bff150de69fc1d
Marenthe de Jong <110374@st-maartenscollege.nl>
2021-05-12 20:30:56 +0200

    Validatie afgemaakt: er wordt nu een errormessage getoond, die nu ook nog gepersonaliseerd is.

--- Models/Person.cs
+++ Models/Person.cs
@@ -4,11 +4,11 @@ namespace Bioscoop_Website_2021.Models
 {
     public class Person
     {
-        [Required]
+        [Required(ErrorMessage = "U bent verplicht uw voornaam in te vullen")]
         public string Voornaam { get; set; }
-        [Required]
+        [Required(ErrorMessage = "U bent verplicht uw achternaam in te vullen")]
         public string Achternaam { get; set; }
-        [Required]
+        [Required(ErrorMessage = "U bent verplicht uw emailadres in te vullen")]
         public string Email { get; set; }
     }
 
