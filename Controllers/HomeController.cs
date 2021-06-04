77e225118274a1d59aa91b740dc01ef88ee2911e
110370 <110370@st-maartencollege.nl>
2021-06-04 12:12:01 +0200

    Detailpagina en route voor detailpagina verwijderd

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,8 +17,8 @@ namespace Bioscoop_Website_2021.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         // stel in waar de database gevonden kan worden
-       private readonly string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=110370;Uid=110370;Pwd=inf2021sql;";
-        //private readonly string connectionString = "Server=172.16.160.21;Port=3306;Database=110370;Uid=110370;Pwd=inf2021sql;";
+        //private readonly string connectionString = "Server=informatica.st-maartenscollege.nl;Port=3306;Database=110370;Uid=110370;Pwd=inf2021sql;";
+        private readonly string connectionString = "Server=172.16.160.21;Port=3306;Database=110370;Uid=110370;Pwd=inf2021sql;";
         // link voor in de les op school
 
         public HomeController(ILogger<HomeController> logger)
@@ -130,12 +130,6 @@ namespace Bioscoop_Website_2021.Controllers
 
             // stop de namen in de html
             return View(film);
-        }
-
-        [Route("Detailpagina")]
-        public IActionResult Detailpagina()
-        {
-            return View();
         }
 
         [Route("Bestelpagina")]
